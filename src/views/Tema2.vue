f5f3bbd58956b77f67463855e70a595ec952bfe6
john <83730668+DevJohn2021@users.noreply.github.com>
2021-09-22 18:13:04 -0500

    tem3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -4,30 +4,203 @@
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
     .titulo-principal
-      .h3 Producción Audiovisual
-    
-    #t_2_1.titulo-segundo
-      .h4 2.1 Infografía Animada
-    
+      .titulo-principal__numero
+        span 2
+      h1  Marco normativo de la tanatopraxia
 
-    #t_2_2.titulo-segundo
-      .h4 2.2 Video Motion
+    p.mb-4 
+      b Un embalsamador tiene muchos roles 
+      b-none que desempeñar dentro de su 
+      b trabajo como tanatopráctor, 
+      b-none tales como:
 
+    .row.justify-content-center.mb-2
+      .col-lg-10
+        .row.justify-content-center
+          .col-auto.imagen-custom2.mb-4.p-lg-2.p-xl-1
+            figure
+              img.imagen-custom1.mx-auto(src='@/assets/curso/tema2/img1.svg', alt='Organizador', style='width:218px')
+          .col-auto.imagen-custom2.mb-4.p-lg-2.p-xl-1
+            figure
+              img.imagen-custom1.mx-auto(src='@/assets/curso/tema2/img2.svg', alt='Embalsamador', style='width:218px')
+          .col-auto.imagen-custom2.mb-4.p-lg-2.p-xl-1
+            figure
+              img.imagen-custom1.mx-auto(src='@/assets/curso/tema2/img3.svg', alt='Acompañar a las familias', style='width:218px')
+          .col-auto.imagen-custom2.mb-4.p-lg-2.p-xl-1
+            figure
+              img.imagen-custom1.mx-auto(src='@/assets/curso/tema2/img4.svg', alt='Conducir y atender el servicio funeral', style='width:218px')
+          
+    p.mb-5 Todo esto solo por su compromiso con el servicio a otros; pero para ello debe conocer la regulación de la profesión y contar con actitudes e insumos que le permitan el desarrollo de estas actividades. Con este apartado del componente, podrá tener más claridad sobre lo mencionado anteriormente.
+    .bloque-texto-a.reverse.color-secundario.p-4.p-md-4.mb-5 
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-9.mb-4.mb-lg-0
+          .bloque-texto-a__texto.p-4
+            p.mb-2 En Colombia, la 
+              b labor de tanatopráctor 
+              b-none no se encuentra regulada apropiadamente, debido a que no existen leyes ni decretos que hablen específicamente del rol del embalsamador, sus características o los requisitos para desempeñarse. Sin embargo, esta labor puede adherirse a diferentes estatutos que sirven como base para su realización y su ejecución. 
+              b Entre ellos, se encuentran:
+        .col-lg-3.mb-4.mb-lg-0.align-self-center
+          figure
+            img.mx-auto(src='@/assets/curso/tema2/img5.svg', alt='imagen ambiental', style='width:276px;')
+        
 
-    #t_2_3.titulo-segundo
-      .h4 2.3 Motion + Presentador
+    .row.justify-content-center
+      .col-lg-5.mb-4
+        figure
+          img.imagen-custom5.mx-auto(src='@/assets/curso/tema2/img6.svg', alt='tanatopractor labor no regulada en colombia', style='')
+      .col-lg-7.mb-4
+        LineaTiempoD.color-primario
+          .row(numero="1" titulo="Ley 9 de 1979")
+            .col-md-12.mb-4.mb-md-0
+              p.mb-4 En esta ley, se encontrará la base necesaria para la aplicación de tanatopraxia. El título IV, Saneamiento de Edificaciones, servirá como guía para conocer algunas de las disposiciones necesarias, con las cuales debe de cumplir cualquier #[strong laboratorio de tanatopraxia a nivel nacional.]
+              p.mb-4 La ley establece las normas #[strong sanitarias para la prevención y control de los agentes biológicos,] físicos o químicos que alteran las características del ambiente exterior de las edificaciones hasta hacerlo peligroso para la salud humana. 
+              .tarjeta-avatar-b.mb-2
+                .tarjeta-avatar-b__img
+                  img(src='@/assets/curso/tema2/icon1.svg' alt='AvatarTop')
+                .tarjeta.bg-line2(style="background-color:#fff;")
+                  .p-4 
+                    p.mb-4 En el título IX “Defunciones, traslado de cadáveres, inhumacion, exhumacion, transporte, transplante y control de especímenes, se encontrará la información necesaria para la verificación de los documentos necesarios para certificar la muerte, certificado de defunción, sin el cual el embalsamador no podrá realizar su labor.
+                    p.mb-4 Para más información, haga clic en el siguiente enlace.
+                    a.anexo.mb-4.mb-lg-0(href="https://www.minsalud.gov.co/Normatividad_Nuevo/LEY%200009%20DE%201979.pdf" target="_blank" style="")
+                      .anexo__icono
+                        img(src="@/assets/template/icono-link.svg")
+                      .anexo__texto
+                        p.color-texto #[strong Enlace web.] Ley 9 de 1979
+          
+          .row(numero="2" titulo="Decreto 391 de 1991")
+            .col-md-12.mb-4.mb-md-4
+              p.mb-4 Por medio de este decreto, el #[strong Distrito Capital de Bogotá] reglamenta el trámite para la inscripción y obtención de la licencia sanitaria de funcionamiento para las funerarias y agencias mortuorias.
+              .row.justify-content-center
+                .col
+                  p.mb-4 Para más información, haga clic en el siguiente enlace.
+                  a.anexo.mb-4.mb-lg-0(href="https://www.alcaldiabogota.gov.co/sisjur/normas/Norma1.jsp?i=1863&dt=S" target="_blank" style="background-color:#fff;")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p.color-texto #[strong Enlace web.] Decreto 391 de 1991
+                .col-auto
+                  figure
+                    img.mx-auto(src='@/assets/curso/tema2/img7.svg' alt='imagen ambiental', style="width:90px;")
+              
+          .row(numero="3" titulo="Norma Técnica Colombiana 5840 del Icontec")
+            .col-md-12.mb-4.mb-md-0
+              p.mb-4 En su #[strong título 5.2.6 Tanatopraxia,] menciona los aspectos básicos para el tratamiento del cadáver y especificaciones que el trabajador debe de cumplir para la realización de esta actividad. Sus generalidades son:
+              ul.lista-ul.ms-md-4
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Comprobar el uso adecuado de todos los elementos de protección personal, para controlar el riesgo al cual se encuentra expuesto.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Revisar la documentación e identificación del fallecido.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Realizar el inventario de las pertenencias del fallecido y determinar su disposición final.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Realizar inspección física al cadáver para determinar su estado.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Reporte del estado del cadáver.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Desinfección del cadáver.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Planeación del proceso de tanatopraxia según las condiciones del cadáver y exigencias de la familia.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Realización del proceso de tanatopraxia.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Vestir el cadáver.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Realización del proceso de tanatoestética.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Verificación del estado del cofre o ataúd para la disposición del cadáver.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Entregar el cadáver al personal encargado.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Diligenciamiento del informe de embalsamamiento.
+                li 
+                  i.fas.fa-biohazard(style="color: #D93328;")
+                  p.mb-0 Desinfección del área de trabajo y materiales.
+              
+          .row(numero="4" titulo="Norma 260601020 del 2017")
+            .col-md-12.mb-4.mb-md-0
+              p.mb-4 Esta norma es un lineamiento desarrollado entre el #[strong SENA y la Mesa Sectorial de Servicios Funerarios,] para certificar las competencias de los tanatopráctores en funciones de embalsamamiento, en el contexto colombiano. 
+              p.mb-4 Para más información, haga clic en el siguiente enlace y busque la #[strong Norma 260601020]  para su lectura.
+              .row.justify-content-center
+                .col
+                  a.anexo.mb-4.mb-lg-0(href="https://certificados.sena.edu.co/claborales/default.asp#resultado" target="_blank" style="background-color:#fff;")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p.color-texto #[strong Enlace web.] Embalsamar el cadáver de acuerdo con técnicas de tanatopraxia y requerimientos del cliente
+                .col-auto
+                  figure
+                    img.mx-auto(src='@/assets/curso/tema2/img8.svg' alt='imagen ambiental', style="width:90px;")
+              
+          .row(numero="5" titulo="Resolución 181331 de 2009")
+            .col-md-12.mb-4.mb-md-4
+              p.mb-4 Esta resolución describe las #[strong características de adecuación de las instalaciones,] a nivel de iluminación, que contribuya a un adecuado desarrollo de las actividades laborales. Si bien la resolución no especifica el contexto del sector funerario, esta se adopta como un lineamiento para el uso y manejo de la electricidad en el proceso de tanatopraxia y otros relacionados con ella.
+              .row.justify-content-center
+                .col-auto
+                  figure
+                    img.mx-auto(src='@/assets/curso/tema2/img9.svg' alt='imagen ambiental', style="width:130px;")
+                .col
+                  p.mb-4 Para más información, haga clic en el siguiente enlace para acceder a la resolución mencionada. 
+                  a.anexo.mb-4.mb-lg-0(href="https://www.minenergia.gov.co/documents/10180/23931303/RES181331_2009.pdf/53f7e906-94de-4cba-959d-ef45b04e93c6" target="_blank" style="background-color:#fff;")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p.color-texto #[strong Enlace web.] Ministerio de Minas y Energía. Resolución Número 181331
+                
 
+          .row(numero="6" titulo="Resolución 5194 de 2010")
+            .col-md-12.mb-4.mb-md-4
+              p.mb-4 En la presente resolución del  #[strong Ministerio de la Protección Social,] se reglamenta la prestación de los servicios de cementerios, inhumación, exhumación y cremación de cadáveres.
+              p.mb-4 Esta resolución expone #[strong parámetros sanitarios que se deben considerar en las prácticas funerarias.] Cabe mencionar que no se menciona la tanatopraxia literalmente, pero  se deben considerar los aspectos que se señalan allí en relación con la inhumación y exhumación, en el momento del embalsamamiento de un cuerpo fallecido.
+              .row.justify-content-center
+                .col
+                  p.mb-4 Para más información, haga clic en el siguiente enlace y consulte la resolución mencionada. 
+                  a.anexo.mb-4.mb-lg-0(href="http://www.saludcapital.gov.co/DDS/Documents/RESOLUCI%C3%93N%205194%20DE%202010%20INHUMACIONES%20EXHUMACIONES%20Y%20CREMACIONES.pdf" target="_blank" style="background-color:#fff;")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p.color-texto #[strong Enlace web.] Ministerio de la Protección Social. Resolución 5194 de 2010
+                
+                .col-auto
+                  figure
+                    img.mx-auto(src='@/assets/curso/tema2/img10.svg' alt='imagen ambiental', style="width:130px;")
+              
 
-    #t_2_4.titulo-segundo
-      .h4 2.4 Animación 2D
+          .row(numero="7" titulo="Circular 28 de 2004")
+            .col-md-12.mb-4.mb-md-4
+              p.mb-4 En la presente circular, se imparten las directrices a las funerarias, salas de velación o agencias mortuorias que presten servicios dentro del 
+                b Distrito Capital, 
+                b-none relacionadas con las 
+                b condiciones higiénico-sanitarias que deben cumplir, 
+                b-none respecto de la implementación de programas de saneamiento básico que incluyan control vectorial, lavado y desinfección de tanques de reserva y almacenamiento de agua; aprobación de la autoridad competente para los establecimientos que cuenten con 
+                b laboratorios de tanatopraxia.
+              .row.justify-content-center
+                .col-auto
+                  figure
+                    img.mx-auto(src='@/assets/curso/tema2/img11.svg' alt='imagen ambiental', style="width:130px;")
+                .col
+                  p.mb-4 Para más información, haga clic en el siguiente enlace y consulte la Circular mencionada. 
+                  a.anexo.mb-4.mb-lg-0(href="https://www.alcaldiabogota.gov.co/sisjur/normas/Norma1.jsp?i=16763" target="_blank" style="background-color:#fff;")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p.color-texto #[strong Enlace web.] Circular 28 de 2004
+                
+              
+              
 
-
-    #t_2_5.titulo-segundo
-      .h4 2.5 Video interactivo
-
-
-    #t_2_6.titulo-segundo
-      .h4 2.6 Infografías
 
 
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,7 +2,118 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        span 3
+      h1  Seguridad y salud en el trabajo de la tanatopraxia
+
+
+    .row.justify-content-center.mb-3
+      .col-lg-auto.mb-4
+        figure
+          img.mx-auto(src='@/assets/curso/tema3/img1.svg', alt='Protocolos de bioseguridad', style='width:450px')
+      .col.mb-4
+        p.mb-4 Toda actividad laboral conlleva un riesgo, sin importar la función que se realice. Esta premisa está presente dentro de las funciones relacionadas con la 
+          b tanatopraxia, 
+          b-none porque el personal está expuesto a riesgos laborales, que se disminuyen con los 
+          b protocolos de bioseguridad. 
+          b-none Según el 
+          b Decreto 1295 de 1994, el riesgo profesional es una situación potencial de peligro que se produce como consecuencia directa o indirecta del trabajo o labor que se desempeña.  
+          b-none El hecho puede llegar a poner en peligro la salud y estabilidad física o psicológica de los trabajadores y afectar su desempeño laboral.
+        .cajon.color-secundario.p-4.mb-4
+          p.mb-0 En el contexto de la 
+            b función de los empleados denominados tanatopráctores, 
+            b-none se pueden clasificar los riesgos laborales así:
+
+    .row.justify-content-center.mb-5
+      .col-auto.mb-4.margen-tarjetaLarga.px-lg-1
+        .tarjeta.tarjeta-container2.tarjeta-oscura
+          .tarjetaContent2
+            .tarjetaImg2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/num1.svg", alt="icono-Numero", style="width:86px;").m-auto
+              p.mb-0 #[strong Riesgo químico ]
+            .tarjetaTxt2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/img2.jpg", alt="imagen ambiental", style="width:238px;").m-auto
+              .px-3
+                .bg-num1
+                  p.mb-2 #[strong Riesgo químico ]
+                  p.mb-0 Este riesgo está relacionado directamente con líquidos y demás sustancias, como polvos y vapores, que se usan en el proceso de embalsamamiento.
+                
+      .col-auto.mb-4.margen-tarjetaLarga.px-lg-1
+        .tarjeta.tarjeta-container2.tarjeta-verde
+          .tarjetaContent2
+            .tarjetaImg2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/num2.svg", alt="icono-Numero", style="width:86px;").m-auto
+              p.mb-0 #[strong Riesgo físico ]
+            .tarjetaTxt3
+              figure.mb-3
+                img(src="@/assets/curso/tema3/img2.jpg", alt="imagen ambiental", style="width:238px;").m-auto
+              .px-3
+                .bg-num2
+                  p.mb-2 #[strong Riesgo físico ]
+                  p.mb-0 Este riesgo está desencadenado por la falta de iluminación, fatiga y ruido que se pueda generar dentro del laboratorio de tanatopraxia, ya que pueden llegar a generar un accidente.
+                
+      .col-auto.mb-4.margen-tarjetaLarga.px-lg-1
+        .tarjeta.tarjeta-container2.tarjeta-oscura
+          .tarjetaContent2
+            .tarjetaImg2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/num3.svg", alt="icono-Numero", style="width:86px;").m-auto
+              p.mb-0 #[strong Riesgo biológico ]
+            .tarjetaTxt2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/img2.jpg", alt="imagen ambiental", style="width:238px;").m-auto
+              .px-3
+                .bg-num3
+                  p.mb-2 #[strong Riesgo biológico ]
+                  p.mb-0 Cuando una persona se expone a este riesgo, debe entender que los cuerpos tratados en el laboratorio de tanatopraxia pueden contener microorganismos, como virus, bacterias, parásitos u hongos, que representan una amenaza para la salud del tanatopráctor.
+                
+      .col-auto.mb-4.margen-tarjetaLarga.px-lg-1
+        .tarjeta.tarjeta-container2.tarjeta-verde
+          .tarjetaContent2
+            .tarjetaImg2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/num4.svg", alt="icono-Numero", style="width:86px;").m-auto
+              p.mb-0 #[strong Riesgo psicológico ]
+            .tarjetaTxt3
+              figure.mb-3
+                img(src="@/assets/curso/tema3/img2.jpg", alt="imagen ambiental", style="width:238px;").m-auto
+              .px-3
+                .bg-num4
+                  p.mb-2 #[strong Riesgo psicológico ]
+                  p.mb-0 Este riesgo es provocado por aspectos como el ambiente y entorno laboral, que pueden llegar a generar estrés severo y, con el correr del tiempo, pueden generar enfermedades inmunitarias, dermatológicas, respiratorias, cardiovasculares o mentales.
+                
+      .col-auto.mb-4.margen-tarjetaLarga.px-lg-1
+        .tarjeta.tarjeta-container2.tarjeta-oscura
+          .tarjetaContent2
+            .tarjetaImg2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/num5.svg", alt="icono-Numero", style="width:86px;").m-auto
+              p.mb-0 #[strong Riesgo ergonómico ]
+            .tarjetaTxt2
+              figure.mb-3
+                img(src="@/assets/curso/tema3/img2.jpg", alt="imagen ambiental", style="width:238px;").m-auto
+              .px-3
+                .bg-num5
+                  p.mb-2 #[strong Riesgo ergonómico ]
+                  p.mb-0 Este riesgo es originado por una incorrecta ergonomía laboral, provocando un trastorno músculo-esquelético por la actividad física de mover el cadáver.
+                
     
+
+
+
+
+
+
+
+
+
+
+
+
 
 </template>
 
